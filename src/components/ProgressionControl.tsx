e60e56f2a21ee4d356e194e469e7f0d5f0b7f6f1
meganrm <meganrm@gmail.com>
2024-03-12 11:58:28 -0700

    remove function call

--- src/components/ProgressionControl.tsx
+++ src/components/ProgressionControl.tsx
@@ -1,12 +1,18 @@
-import React, { InputHTMLAttributes, useContext } from 'react';
-import { SimulariumContext } from '../simulation/context';
+import React, { InputHTMLAttributes, useContext } from "react";
+import { SimulariumContext } from "../simulation/context";
 
 interface ProgressionControlProps {
-    children: React.ReactElement<InputHTMLAttributes<HTMLInputElement> | InputHTMLAttributes<HTMLButtonElement>>;
+    children: React.ReactElement<
+        | InputHTMLAttributes<HTMLInputElement>
+        | InputHTMLAttributes<HTMLButtonElement>
+    >;
     onPage: number;
 }
 
-const ProgressionControl: React.FC<ProgressionControlProps> = ({children, onPage}) => {
+const ProgressionControl: React.FC<ProgressionControlProps> = ({
+    children,
+    onPage,
+}) => {
     const { page, setPage } = useContext(SimulariumContext);
     const progress = () => {
         if (page === onPage) {
@@ -15,42 +21,48 @@ const ProgressionControl: React.FC<ProgressionControlProps> = ({children, onPage
     };
 
     const mergeHandlers = (
-        baseHandler: React.MouseEventHandler<HTMLButtonElement> | React.ChangeEventHandler<HTMLInputElement>
+        baseHandler:
+            | React.MouseEventHandler<HTMLButtonElement>
+            | React.ChangeEventHandler<HTMLInputElement>
     ) => {
-        return (event: React.MouseEvent<HTMLButtonElement> | React.ChangeEvent<HTMLInputElement>) => {
+        return (
+            event:
+                | React.MouseEvent<HTMLButtonElement>
+                | React.ChangeEvent<HTMLInputElement>
+        ) => {
             progress();
-            baseHandler(event as React.MouseEvent<HTMLButtonElement> & React.ChangeEvent<HTMLInputElement>);
+            baseHandler(
+                event as React.MouseEvent<HTMLButtonElement> &
+                    React.ChangeEvent<HTMLInputElement>
+            );
         };
     };
 
-    const renderChildren = () => {
-         return React.Children.map(children, (child) => {
-            if (!React.isValidElement(child)) {
-                return child;
-            }
-            const reactElement = child as React.ReactElement;
-            if (child.props.onClick) {
-                return React.cloneElement(reactElement, {
-                    onClick: mergeHandlers(
-                        child.props.onClick as React.MouseEventHandler<HTMLButtonElement>
-                    ),
-                });
-            } else if (child.props.onChange) {
-                return React.cloneElement(reactElement, {
-                    onChange: mergeHandlers(
-                        child.props.onChange as React.ChangeEventHandler<HTMLInputElement>
-                    ),
-                });
-            }
-
+    return React.Children.map(children, (child) => {
+        if (!React.isValidElement(child)) {
+            return child;
+        }
+        const reactElement = child as React.ReactElement;
+        if (child.props.onClick) {
             return React.cloneElement(reactElement, {
-                onClick: progress,
+                onClick: mergeHandlers(
+                    child.props
+                        .onClick as React.MouseEventHandler<HTMLButtonElement>
+                ),
             });
-         });
-     }; 
+        } else if (child.props.onChange) {
+            return React.cloneElement(reactElement, {
+                onChange: mergeHandlers(
+                    child.props
+                        .onChange as React.ChangeEventHandler<HTMLInputElement>
+                ),
+            });
+        }
 
-
-    return <>{renderChildren()}</>;
+        return React.cloneElement(reactElement, {
+            onClick: progress,
+        });
+    });
 };
 
 export default ProgressionControl;
