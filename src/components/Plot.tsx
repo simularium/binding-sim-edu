0a3ff84af95ab3ecb4fb225ee70ba2672ee019de
Megan Riel-Mehan <meganrm@gmail.com>
2024-03-13 10:40:43 -0700

    Feature/plot colors (#11)
    * add some colors so plots don't change color
    
    * on mouse up

--- src/components/Plot.tsx
+++ src/components/Plot.tsx
@@ -7,6 +7,17 @@ interface PlotProps {
     data: { [key: string]: number[] };
 }
 
+const PLOT_COLORS = [
+    "#a6cee3",
+    "#b2df8a",
+    "#33a02c",
+    "#fb9a99",
+    "#1f78b4",
+    "#e31a1c",
+    "#fdbf6f",
+    "#ff7f00",
+    "#cab2d6",
+];
 const LinePlot: React.FC<PlotProps> = ({ data }) => {
     const traces = map(
         data,
@@ -17,20 +28,24 @@ const LinePlot: React.FC<PlotProps> = ({ data }) => {
                 // already but not necessarily data yet 
                 return {};
             }
+            const colorNumber = Number(id) % PLOT_COLORS.length;
             return {
                 x: yValues.map((_, i) => i),
                 y: yValues,
                 type: "scatter" as const,
                 mode: "lines" as const,
                 name: id,
+                line: { color: PLOT_COLORS[colorNumber] },
             };
         }
     );
 
     const layout = {
         title: "Concentration over Time",
-        xaxis: { title: "Time", range: [0, "auto"] },
-        yaxis: { title: "Concentration" },
+        xaxis: { title: "time (ns)", range: [0, "auto"], color: "#D3D3D3" },
+        yaxis: { title: "[AB]", color: "#D3D3D3" },
+        paper_bgcolor: "#141219",
+        plot_bgcolor: "#141219",
     };
 
     return <Plot data={traces} layout={layout} />;
