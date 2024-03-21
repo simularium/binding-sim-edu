87db28a7c73c9823fb0e83a7029c25279d313149
meganrm <meganrm@gmail.com>
2024-03-21 14:25:40 -0700

    add success message

--- src/components/EquilibriumQuestion.tsx
+++ src/components/EquilibriumQuestion.tsx
@@ -1,21 +1,30 @@
 import React, { useState } from "react";
 import QuizForm from "./QuizForm";
 import VisibilityControl from "./VisibilityControl";
+import SuccessFeedback from "./SuccessFeedback";
 
 const EquilibriumQuestion: React.FC = () => {
     const [selectedAnswer, setSelectedAnswer] = useState("");
+    const [submitButtonLabel, setSubmitButtonLabel] = useState("Submit");
+    const [success, setSuccess] = useState(false);
 
     const handleAnswerSelection = (answer: string) => {
         setSelectedAnswer(answer);
     };
 
     const handleSubmit = () => {
+        if (submitButtonLabel === "Try Again") {
+            setSubmitButtonLabel("Submit");
+            setSelectedAnswer("");
+            return;
+        }
         if (selectedAnswer === "C") {
-            console.log("Correct!");
+            setSuccess(true);
         } else {
-            console.log("Incorrect");
+            setSubmitButtonLabel("Try Again");
         }
     };
+
     const answers = [
         { value: "A", label: "binding reactions stop happening" },
         { value: "B", label: "unbinding reactions stop happening" },
@@ -26,26 +35,46 @@ const EquilibriumQuestion: React.FC = () => {
         { value: "D", label: "the amount of products and reactants are equal" },
     ];
 
-    const formContent = answers.map((answer, index) => (
-        <li key={index}>
-            <label>
-                <input
-                    type="radio"
-                    value={answer.value}
-                    checked={selectedAnswer === answer.value}
-                    onChange={() => handleAnswerSelection(answer.value)}
-                />
-                {answer.label}
-            </label>
-        </li>
-    ));
+    const formContent = (
+        <>
+            <ul>
+                {answers.map((answer, index) => (
+                    <li key={index}>
+                        <label>
+                            <input
+                                type="radio"
+                                value={answer.value}
+                                checked={selectedAnswer === answer.value}
+                                onChange={() =>
+                                    handleAnswerSelection(answer.value)
+                                }
+                            />
+                            {answer.label}
+                        </label>
+                    </li>
+                ))}
+            </ul>
+            {submitButtonLabel === "Try Again" && <p>Sorry that's incorrect</p>}
+        </>
+    );
+    // TODO: this should only be shown at 4, but need to merge other changes before we can 
+    // get to page 4 
     return (
-        <VisibilityControl includedPages={[1]}>
-            <QuizForm
-                title="Which of the following is true about equilibrium?"
-                formContent={formContent}
-                onSubmit={handleSubmit}
-            />
+        <VisibilityControl includedPages={[2, 4]}>
+            {success ? (
+                <SuccessFeedback
+                    message={
+                        "It’s the forward and reverse reaction rates that are equal at equilibrium, not the concentrations of reactants and products. And binding and unbinding events don’t stop happening."
+                    }
+                />
+            ) : (
+                <QuizForm
+                    title="Which of the following is true about equilibrium?"
+                    formContent={formContent}
+                    onSubmit={handleSubmit}
+                    submitButtonLabel={submitButtonLabel}
+                />
+            )}
         </VisibilityControl>
     );
 };
--- src/components/QuizForm.tsx
+++ src/components/QuizForm.tsx
@@ -4,17 +4,23 @@ interface QuizFormProps {
     title: string;
     formContent: JSX.Element | JSX.Element[];
     onSubmit: () => void;
+    submitButtonLabel?: string;
 }
 
 const QuizForm: React.FC<QuizFormProps> = ({
     title,
     formContent,
     onSubmit,
+    submitButtonLabel = "Submit",
 }) => {
     const [isFormVisible, setIsFormVisible] = useState(true);
 
     const toggleFormVisibility = () => {
         setIsFormVisible(!isFormVisible);
+    };
+
+    const handleSubmit = () => {
+        onSubmit();
     };
 
     return (
@@ -24,7 +30,7 @@ const QuizForm: React.FC<QuizFormProps> = ({
             <button onClick={toggleFormVisibility}>
                 {isFormVisible ? "Hide Form" : "Show Form"}
             </button>
-            <button onClick={onSubmit}>Submit</button>
+            <button onClick={() => handleSubmit()}>{submitButtonLabel}</button>
         </div>
     );
 };
