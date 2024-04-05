619e02a4c44f1fcf647008a53e0287491b7e7b73
meganrm <meganrm@gmail.com>
2024-04-05 10:47:58 -0700

    position admin ui over the app

--- src/components/AdminUi.tsx
+++ src/components/AdminUi.tsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Slider from "./shared/Slider";
+import { BG_DARK, LIGHT_GREY } from "../constants/colors";
 
 interface AdminUIProps {
     timeFactor: number;
     setTimeFactor: (value: number) => void;
 }
 
-const AdminUI: React.FC<AdminUIProps> = ({timeFactor, setTimeFactor}) => {
+const AdminUI: React.FC<AdminUIProps> = ({ timeFactor, setTimeFactor }) => {
+    const [visible, setVisible] = React.useState<boolean>(false);
+
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            // control-option-1 (mac) or ctrl-alt-1 (windows)
+            if (event.code === "Digit1" && event.altKey && event.ctrlKey) {
+                setVisible(!visible);
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [visible]);
 
     return (
-        <div>
-            <h1>AdminUI</h1>
-            <Slider
-                min={0}
-                max={100}
-                initialValue={timeFactor}
-                onChange={(_, value) => {
-                    setTimeFactor(value);
-                }}
-                disabled={false}
-                name="time factor (ns)"
-            />
-        </div>
+        visible && (
+            <div style={{position: "absolute", bottom: 0, background: LIGHT_GREY, color: BG_DARK}}>
+                <h1>AdminUI</h1>
+                <Slider
+                    min={0}
+                    max={100}
+                    initialValue={timeFactor}
+                    onChange={(_, value) => {
+                        setTimeFactor(value);
+                    }}
+                    disabled={false}
+                    name="time factor (ns)"
+                />
+            </div>
+        )
     );
 };
 
