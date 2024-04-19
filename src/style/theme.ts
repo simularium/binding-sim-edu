280efeea8cf72259d2187fec3c49bc9a345231e1
Megan Riel-Mehan <meganrm@gmail.com>
2024-04-18 17:23:29 -0700

    Feature/style content banner (#41)
    * add dark theme algo
    
    * added fonts and text style
    
    * style paragraphs and add icon
    
    * remove default text margins
    
    * add border

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -47,6 +47,7 @@ const customTheme = {
             trackHoverBg: MID_GREY_DARKER,
             handleColor: BRIGHT_PURPLE_DARKER,
             handleActiveColor: BRIGHT_PURPLE_DARKER,
+            handleLineWidthHover: 0,
         },
     },
 };
