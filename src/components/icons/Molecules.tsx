4320ca035f8dd52962a18ac41f1037b039fb8691
meganrm <meganrm@gmail.com>
2024-04-19 12:05:31 -0700

    use customColor for styling icons

--- src/components/icons/Molecules.tsx
+++ src/components/icons/Molecules.tsx
@@ -12,23 +12,32 @@ const Molecules: React.FC = () => {
         >
             <path
                 d="M3 17.5L4.23092 13.2692L7.5 16L9.5 10"
-                strokeWidth="0.75"
+                stroke="currentColor"
             />
-            <ellipse cx="2.5" cy="18.7307" rx="2.5" ry="2.5" />
+            <ellipse
+                cx="2.5"
+                cy="18.7307"
+                rx="2.5"
+                ry="2.5"
+                fill="currentColor"
+            />
             <path
                 d="M6.5 3.5L12.2691 2.11542L13.5383 6.76927L19.4614 4.65388L16.4998 0.846191"
                 strokeWidth="0.75"
+                stroke="currentColor"
             />
-            <circle cx="3.75" cy="4.6001" r="3.75"  />
+            <circle cx="3.75" cy="4.6001" r="3.75" fill="currentColor" />
             <path
                 d="M17.5 12.5L20.3078 17.7691L15.2308 21.1537L12.6924 16.0768L20.3078 20.7307"
                 strokeWidth="0.75"
+                stroke="currentColor"
             />
             <ellipse
                 cx="16.8848"
                 cy="10.9614"
                 rx="2.5"
                 ry="2.5"
+                fill="currentColor"
             />
         </svg>
     );
