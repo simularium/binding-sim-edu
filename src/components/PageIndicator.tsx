3bb83917a1709277b5bab33ee217f4781bd2cc37
Megan Riel-Mehan <meganrm@gmail.com>
2024-04-23 11:03:55 -0700

    Feature page progress bar (#50)
    * increase the default time step
    
    * add relax cycles and parents
    
    * format doc
    
    * rename functions
    
    * rename symbol
    
    * add progress bar
    
    * fix names
    
    * show progress bar
    
    * style star
    
    * cleanup
    
    * move to own file
    
    * fix import
    
    * fix import
    
    * fix font size
    
    * format document
    
    * fine tuning style
    
    * update fonts to include
    
    * cleanup based on comment
    
    * rename for consistency
    
    * remove accidental paste

---
+++ src/components/PageIndicator.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Progress, Flex } from "antd";
+import { map } from "lodash";
+import classNames from "classnames";
+
+import { moduleNames } from "../content";
+import styles from "./page-indicator.module.css";
+
+interface PageIndicatorProps {
+    title: string;
+    page: number;
+    total: number;
+}
+
+const PageIndicator: React.FC<PageIndicatorProps> = ({
+    title,
+    page,
+    total,
+}) => {
+    let indexOfActiveModule = -1;
+
+    const getModulePercent = (isActiveModule: boolean, moduleIndex: number) => {
+        if (isActiveModule) {
+            return (page / total) * 100;
+        } else if (moduleIndex < indexOfActiveModule) {
+            return 100;
+        } else {
+            return 0;
+        }
+    };
+    return (
+        <Flex align="center" justify="flex-end" className={styles.container}>
+            {map(moduleNames, (name, index) => {
+                const moduleIndex = Number(index);
+                const isActiveModule = name === title;
+                if (isActiveModule) {
+                    indexOfActiveModule = moduleIndex;
+                }
+                return (
+                    <div
+                        key={index}
+                        className={classNames(styles.progressBarWrapper, {
+                            [styles.previous]:
+                                moduleIndex <= indexOfActiveModule,
+                            [styles.current]: isActiveModule,
+                        })}
+                    >
+                        <div className={styles.title}>{name}</div>
+                        <Progress
+                            className={styles.progressBar}
+                            strokeWidth={4}
+                            percent={getModulePercent(
+                                isActiveModule,
+                                moduleIndex
+                            )}
+                            showInfo={false}
+                        />
+
+                        <div className={styles.pageNumber}>
+                            {isActiveModule ? `${page} / ${total}` : ""}
+                        </div>
+                    </div>
+                );
+            })}
+        </Flex>
+    );
+};
+
+export default PageIndicator;
