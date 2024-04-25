d880065b6756e8164d1ed05514b2bb18e2040131
Megan Riel-Mehan <meganrm@gmail.com>
2024-04-24 17:25:14 -0700

    size plots correctly and display reaction (#58)
    * increase the default time step
    
    * add relax cycles and parents
    
    * format doc
    
    * rename functions
    
    * rename symbol
    
    * add progress bar
    
    * fix names
    
    * show progress bar
    
    * style star
    
    * cleanup
    
    * move to own file
    
    * fix import
    
    * fix import
    
    * fix font size
    
    * format document
    
    * fine tuning style
    
    * getting live concentrations
    
    * visualize live concentration
    
    * update fonts to include
    
    * style live indicator
    
    * render and style live concentration
    
    * moved functions
    
    * use enum value
    
    * add comment remove a log
    
    * style widths
    
    * style numbers
    
    * clicking slider also updates
    
    * change title font size
    
    * simplify type
    
    * organize concentration
    
    * use shared slider and revert header change
    
    * concentration slider is never disabled
    
    * size plots correct and display reaction
    
    * get rid of grid
    
    * change plot markers color
    
    * change check to eliminate negative steps
    
    * fix import
    
    * clean up agents
    
    * import as components and move style
    
    * keep steps from being negative
    
    * move style to be more general
    
    * use correct type
    
    * fine tuning star position
    
    * remove position
    
    * less hardcoded values
    
    * nudge star down
    
    * disable zero

--- src/components/plots/EventsOverTimePlot.tsx
+++ src/components/plots/EventsOverTimePlot.tsx
@@ -1,7 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { Flex } from "antd";
 import Plot from "react-plotly.js";
-import { BASE_PLOT_LAYOUT, GRAY_COLOR } from "./constants";
+
+import {
+    AXIS_COLOR,
+    BASE_PLOT_LAYOUT,
+    PLOT_BACKGROUND_COLOR,
+} from "./constants";
 import { SimulariumContext } from "../../simulation/context";
+import { A, B, AB } from "../agent-symbols";
+
+import plotStyles from "./plots.module.css";
+import layoutStyles from "./events-over-time.module.css";
 
 interface PlotProps {
     bindingEventsOverTime: number[];
@@ -13,41 +23,94 @@ const EventsOverTimePlot: React.FC<PlotProps> = ({
     unbindingEventsOverTime,
 }) => {
     const { timeFactor } = useContext(SimulariumContext);
+    const [width, setWidth] = useState<number>(0);
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        setWidth(containerRef.current?.offsetWidth || 0);
+    }, [containerRef.current?.offsetWidth, width]);
+
     const layout = {
         ...BASE_PLOT_LAYOUT,
-        xaxis: { range: [0, "auto"] },
+        height: 40,
+        width: width,
+        margin: { l: 0, r: 25.5, b: 0, t: 0 },
+    };
+
+    const axisSettings = {
+        range: [0, "auto"],
+        gridcolor: PLOT_BACKGROUND_COLOR,
+        showticklabels: false,
+        color: AXIS_COLOR,
+        fixedrange: true,
+        showline: true,
     };
     // the two arrays will always be the same length
     // so this time calculation only needs to happen once
-    const time = bindingEventsOverTime.map((_, i) => i * 10 * timeFactor / 1000);
+    const time = bindingEventsOverTime.map(
+        (_, i) => (i * 10 * timeFactor) / 1000
+    );
+
     return (
-        <>
-            <h4>Reaction events over time</h4>
-            <Plot
-                data={[
-                    {
-                        x: time,
-                        y: bindingEventsOverTime,
-                        type: "bar" as const,
-                        name: "bind events",
-                        marker: { color: GRAY_COLOR },
-                    },
-                ]}
-                layout={{ ...layout, title: "A + B -> AB" }}
-            />
-            <Plot
-                data={[
-                    {
-                        x: time,
-                        y: unbindingEventsOverTime,
-                        type: "bar" as const,
-                        name: "unbind events",
-                        marker: { color: GRAY_COLOR },
-                    },
-                ]}
-                layout={{ ...layout, title: "AB -> A + B" }}
-            />
-        </>
+        <div className={layoutStyles.container}>
+            <h3>Reaction events over time</h3>
+            <div className={plotStyles.yLabel}>Count of reactions</div>
+            <Flex
+                className={layoutStyles.plots}
+                vertical
+                gap={8}
+                ref={containerRef}
+            >
+                <div>
+                    <A />
+                    <span> + </span>
+                    <B />
+                    <span> &#8594; </span>
+                    <AB />
+                </div>
+                <Plot
+                    data={[
+                        {
+                            x: time,
+                            y: bindingEventsOverTime,
+                            type: "bar" as const,
+                            name: "bind events",
+                            marker: { color: AXIS_COLOR },
+                        },
+                    ]}
+                    layout={{
+                        ...layout,
+                        xaxis: { ...axisSettings },
+                        yaxis: { ...axisSettings },
+                    }}
+                    config={{ displayModeBar: false }}
+                />
+                <div>
+                    <AB />
+                    <span> &#8594; </span>
+                    <A />
+                    <span> + </span>
+                    <B />
+                </div>
+                <Plot
+                    data={[
+                        {
+                            x: time,
+                            y: unbindingEventsOverTime,
+                            type: "bar" as const,
+                            name: "unbind events",
+                            marker: { color: AXIS_COLOR },
+                        },
+                    ]}
+                    layout={{
+                        ...layout,
+                        xaxis: { ...axisSettings },
+                        yaxis: { ...axisSettings },
+                    }}
+                    config={{ displayModeBar: false }}
+                />
+            </Flex>
+        </div>
     );
 };
 
