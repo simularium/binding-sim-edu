d99261646aea2f8be135618401c36a4e455082e3
Megan Riel-Mehan <meganrm@gmail.com>
2024-04-24 17:22:27 -0700

    Feature/input slider (#56)
    * increase the default time step
    
    * add relax cycles and parents
    
    * format doc
    
    * rename functions
    
    * rename symbol
    
    * add progress bar
    
    * fix names
    
    * show progress bar
    
    * style star
    
    * cleanup
    
    * move to own file
    
    * fix import
    
    * fix import
    
    * fix font size
    
    * format document
    
    * fine tuning style
    
    * getting live concentrations
    
    * visualize live concentration
    
    * update fonts to include
    
    * style live indicator
    
    * render and style live concentration
    
    * moved functions
    
    * use enum value
    
    * add comment remove a log
    
    * style widths
    
    * style numbers
    
    * clicking slider also updates
    
    * change title font size
    
    * simplify type
    
    * organize concentration
    
    * use shared slider and revert header change
    
    * concentration slider is never disabled
    
    * keep steps from being negative
    
    * move style to be more general
    
    * use correct type
    
    * fine tuning star position
    
    * remove position
    
    * less hardcoded values
    
    * nudge star down
    
    * disable zero
    
    * Update src/App.tsx
    
    Co-authored-by: Peyton Lee <peytonalee@gmail.com>
    
    * Update src/App.tsx
    
    Co-authored-by: Cameron Fraser <53030214+frasercl@users.noreply.github.com>
    
    * pulling in changes in main
    
    * simplify line under progress bar
    
    * remove duplication
    
    * remove duplicate files from merging main
    
    ---------
    
    Co-authored-by: Peyton Lee <peytonalee@gmail.com>
    Co-authored-by: Cameron Fraser <53030214+frasercl@users.noreply.github.com>

--- src/components/shared/Slider.tsx
+++ src/components/shared/Slider.tsx
@@ -1,39 +1,54 @@
 import React, { useState } from "react";
-import { Slider as SliderAntd } from "antd";
+import { Slider as SliderAntd, SliderSingleProps } from "antd";
 
 interface SliderProps {
     min: number;
     max: number;
+    step: number;
     initialValue: number;
     onChange: (name: string, value: number) => void;
     name: string;
     disabled?: boolean;
+    marks?: SliderSingleProps["marks"];
+    className?: string;
+    disabledNumbers?: number[]
 }
 
 const Slider: React.FC<SliderProps> = ({
     min,
     max,
+    step,
     initialValue,
     onChange,
     name,
     disabled,
+    marks,
+    className,
+    disabledNumbers
 }) => {
     const [value, setValue] = useState(initialValue);
+    const handleSliderChange = (newValue: number) => {
+        if (disabledNumbers && disabledNumbers.includes(newValue)) {
+            setValue(value);
 
-    const handleSliderChange = (value: number) => {
-        setValue(value);
-        onChange(name, value);
+            return;
+        }
+        setValue(newValue);
+        onChange(name, newValue);
     };
 
     return (
         <SliderAntd
+            className={className}
             min={min}
             max={max}
             style={{ width: "100%" }}
-            step={2}
+            step={step}
             value={value}
             onChange={handleSliderChange}
+            onChangeComplete={handleSliderChange}
             disabled={disabled}
+            marks={marks}
         />
     );
 };
