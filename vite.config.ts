bb12f9216d731c48380b1d33dcab93610f02231c
meganrm <meganrm@gmail.com>
2024-04-29 18:05:14 -0700

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/learn/binding-affinity/",
+    base: "./learn/binding-affinity/",
     plugins: [react()],
     css: {
         modules: {
