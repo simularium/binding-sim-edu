fe92fbdf780fbceeefd1cdb46134142245da7e51
meganrm <meganrm@gmail.com>
2024-04-29 18:06:54 -0700

    change base to full url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./learn/binding-affinity/",
+    base: "https://staging.simularium.allencell.org/learn/binding-affinity",
     plugins: [react()],
     css: {
         modules: {
