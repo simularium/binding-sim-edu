9974a77dc957ecd0f91e44ff1e0c38baf15316cb
meganrm <meganrm@gmail.com>
2024-04-29 18:22:05 -0700

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://staging.simularium.allencell.org/learn/binding-affinity",
     plugins: [react()],
     css: {
         modules: {
