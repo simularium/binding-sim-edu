27d18b639beecfd3c592a8b5d457c723c399fe4e
meganrm <meganrm@gmail.com>
2024-04-30 15:01:32 -0700

    add comment

--- src/hooks/useWindowResize.ts
+++ src/hooks/useWindowResize.ts
@@ -3,6 +3,8 @@ import { useEffect, useRef } from 'react';
 const useWindowResize = (callback: () => void) => {
     const resizeEvent = useRef<NodeJS.Timeout | undefined>(undefined);
     useEffect(() => {
+        // creates an event listener that only 
+        // calls the callback after the user has stopped resizing the window
         window.addEventListener("resize", () => {
             clearTimeout(resizeEvent.current);
             resizeEvent.current = setTimeout(() => {
