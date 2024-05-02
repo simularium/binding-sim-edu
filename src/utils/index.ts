f2772aaf2a5fb1072b317e4279d6715325f7c06d
meganrm <meganrm@gmail.com>
2024-05-02 15:22:22 -0700

    return types

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,17 @@
 import { sortedIndex } from "lodash";
 
-export const insertIntoArray = (
-    array: unknown[],
+export const insertIntoArray = <T>(
+    array: T[],
     index: number,
-    value: unknown
-) => {
+    value: T
+): T[] => {
     return [...array.slice(0, index), value, ...array.slice(index)];
 };
 
-export const insertValueSorted = (array: number[], value: number) => {
+export const insertValueSorted = (
+    array: number[],
+    value: number
+): { newArray: number[]; index: number } => {
     const index = sortedIndex(array, value);
     return { newArray: insertIntoArray(array, index, value), index };
 };
