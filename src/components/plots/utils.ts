39c08875c2de0574ecbeebb09fb7bda374f47cf8
meganrm <meganrm@gmail.com>
2024-05-03 10:46:49 -0700

    add math floor

--- src/components/plots/utils.ts
+++ src/components/plots/utils.ts
@@ -1,5 +1,5 @@
 import { PLOT_COLORS } from "./constants";
 
 export const getColorIndex = (value: number | string, maxValue: number) => {
-    return ((Number(value) / maxValue) * 10) % PLOT_COLORS.length;
+        return Math.floor(((Number(value) / maxValue) * 10)) % PLOT_COLORS.length;
 };
