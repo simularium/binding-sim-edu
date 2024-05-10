8a24ba8a35e22ddd3af99b0d1b1b7583a0f8c6b7
meganrm <meganrm@gmail.com>
2024-05-10 12:52:51 -0700

    fix import

--- src/components/main-layout/CenterPanel.tsx
+++ src/components/main-layout/CenterPanel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ViewSwitch from '../ViewSwitch';
 import EquilibriumQuestion from '../quiz-questions/EquilibriumQuestion';
 import KdQuestion from '../quiz-questions/KdQuestion';
-import { Module } from '../../constants';
+import { Module } from '../../types';
 
 interface CenterPanelProps {
     reactionType: Module;
