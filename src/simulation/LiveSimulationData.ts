27a0cf3dfba6f7b20af121962300bb24bd095a42
Megan Riel-Mehan <meganrm@gmail.com>
2024-06-05 15:18:28 -0700

    Feature/3d trajectory data (#86)
    * clean up concentration change
    
    * save time
    
    * wip concentrations for trajectory
    
    * using interface to load trajectory data
    
    * remove log
    
    * clean up interfaces
    
    * add time unit
    
    * reverse change
    
    * use isLastFrame
    
    * fix steps
    
    * remove change
    
    * move setting plot data after changeFile
    
    * play and pause working on final trajectory
    
    * add rangemode
    
    * change name
    
    * change timefactor back
    
    * change to constant

---
+++ src/simulation/LiveSimulationData.ts
@@ -0,0 +1,160 @@
+import {
+    AgentFunction,
+    AgentName,
+    CurrentConcentration,
+    InputAgent,
+    Module,
+    ProductName,
+} from "../types";
+import {
+    AGENT_A_COLOR,
+    AGENT_B_COLOR,
+    AGENT_C_COLOR,
+} from "../constants/colors";
+import ISimulationData, {
+    AGENT_AND_PRODUCT_COLORS,
+    TrajectoryType,
+} from "./ISimulationData";
+import { NANO } from "../constants";
+
+const agentA: InputAgent = {
+    id: 0,
+    name: AgentName.A,
+    initialConcentration: 0,
+    radius: 3,
+    partners: [1, 2],
+    color: AGENT_A_COLOR,
+};
+
+const agentB: InputAgent = {
+    id: 1,
+    name: AgentName.B,
+    initialConcentration: 0,
+    radius: 0.7,
+    partners: [0],
+    kOn: 0.6,
+    kOff: 0.2,
+    color: AGENT_B_COLOR,
+};
+
+const agentC: InputAgent = {
+    id: 2,
+    name: AgentName.C,
+    initialConcentration: 0,
+    radius: 1,
+    partners: [0],
+    kOn: 0.5,
+    kOff: 0.8,
+    color: AGENT_C_COLOR,
+};
+
+export default class LiveSimulation implements ISimulationData {
+    static NAME_TO_FUNCTION_MAP = {
+        [AgentName.A]: AgentFunction.Fixed,
+        [AgentName.B]: AgentFunction.Adjustable,
+        [AgentName.C]: AgentFunction.Competitor,
+        [ProductName.AB]: AgentFunction.Complex,
+        [ProductName.AC]: AgentFunction.Complex,
+    };
+    static DEFAULT_TIME_FACTOR: number = 85;
+    static AVAILABLE_AGENTS = {
+        [AgentName.A]: agentA,
+        [AgentName.B]: agentB,
+        [AgentName.C]: agentC,
+    };
+    static INITIAL_CONCENTRATIONS = {
+        [AgentName.A]: 10,
+        [AgentName.B]: 10,
+        [AgentName.C]: 10,
+    };
+    PRODUCT = {
+        [Module.A_B_AB]: ProductName.AB,
+        [Module.A_C_AC]: ProductName.AC,
+        [Module.A_B_C_AB_AC]: ProductName.AB,
+    };
+
+    timeUnit = NANO;
+
+    type = TrajectoryType.live;
+
+    getCurrentProduct = (module: Module): ProductName => {
+        return this.PRODUCT[module];
+    };
+
+    getAgentFunction = (name: AgentName | ProductName): AgentFunction => {
+        return (
+            LiveSimulation.NAME_TO_FUNCTION_MAP as Record<
+                AgentName | ProductName,
+                AgentFunction
+            >
+        )[name];
+    };
+
+    getAgentColor = (name: AgentName | ProductName): string => {
+        const agentFunction = this.getAgentFunction(name);
+        return AGENT_AND_PRODUCT_COLORS[agentFunction];
+    };
+
+    getMaxConcentration = (module: Module): number => {
+        let maxConcentration = 0;
+        switch (module) {
+            case Module.A_B_AB:
+                maxConcentration = 10;
+                break;
+            case Module.A_C_AC:
+                maxConcentration = 20; //TODO: adjust these as needed
+                break;
+            case Module.A_B_C_AB_AC:
+                maxConcentration = 20; //TODO: adjust these as needed
+                break;
+        }
+        return maxConcentration;
+    };
+
+    createAgentsFromConcentrations = (
+        activeAgents?: AgentName[]
+    ): InputAgent[] => {
+        return (activeAgents ?? []).map((agentName: AgentName) => {
+            const agent = {
+                ...(
+                    LiveSimulation.AVAILABLE_AGENTS as Record<
+                        AgentName,
+                        InputAgent
+                    >
+                )[agentName],
+            };
+            agent.initialConcentration =
+                LiveSimulation.INITIAL_CONCENTRATIONS[
+                    agentName as keyof typeof LiveSimulation.INITIAL_CONCENTRATIONS
+                ];
+            return agent;
+        });
+    };
+
+    getActiveAgents = (currentModule: Module): AgentName[] => {
+        switch (currentModule) {
+            case Module.A_B_AB:
+                return [AgentName.A, AgentName.B];
+            case Module.A_C_AC:
+                return [AgentName.A, AgentName.C];
+            case Module.A_B_C_AB_AC:
+                return [AgentName.A, AgentName.B, AgentName.C];
+            default:
+                return [];
+        }
+    };
+    // filters down to the active agents
+    getInitialConcentrations = (
+        activeAgents: AgentName[]
+    ): CurrentConcentration => {
+        return activeAgents.reduce((acc, agent) => {
+            return {
+                ...acc,
+                [agent]:
+                    LiveSimulation.INITIAL_CONCENTRATIONS[
+                        agent as keyof typeof LiveSimulation.INITIAL_CONCENTRATIONS
+                    ],
+            };
+        }, {});
+    };
+}
---
+++ src/simulation/PreComputedSimulationData.ts
@@ -0,0 +1,85 @@
+import {
+    AgentFunction,
+    AgentName,
+    CurrentConcentration,
+    InputAgent,
+    Module,
+    ProductName,
+} from "../types";
+import ISimulationData, {
+    AGENT_AND_PRODUCT_COLORS,
+    TrajectoryType,
+} from "./ISimulationData";
+import { MICRO } from "../constants";
+
+export default class PreComputedSimulationData implements ISimulationData {
+    static NAME_TO_FUNCTION_MAP = {
+        [AgentName.Antibody]: AgentFunction.Fixed,
+        [AgentName.Antigen]: AgentFunction.Adjustable,
+        [ProductName.AntibodyAntigen]: AgentFunction.Complex,
+    };
+    static EXAMPLE_TRAJECTORY_URLS = {
+        [Module.A_B_AB]:
+            "https://aics-simularium-data.s3.us-east-2.amazonaws.com/trajectory/binding-affinity_antibodies.simularium",
+        [Module.A_C_AC]:
+            "https://aics-simularium-data.s3.us-east-2.amazonaws.com/trajectory/binding-affinity_hemoglobin.simularium",
+        [Module.A_B_C_AB_AC]:
+            "https://aics-simularium-data.s3.us-east-2.amazonaws.com/trajectory/binding-affinity_hemoglobin-co.simularium",
+    };
+
+    timeUnit = MICRO;
+
+    PRODUCT = {
+        [Module.A_B_AB]: ProductName.AntibodyAntigen,
+        [Module.A_C_AC]: ProductName.Hemoglobin,
+        [Module.A_B_C_AB_AC]: ProductName.Hemoglobin,
+    };
+
+    type = TrajectoryType.precomputed;
+
+    getCurrentProduct = (module: Module): ProductName => {
+        return this.PRODUCT[module];
+    };
+
+    getMaxConcentration = (module: Module): number => {
+        let maxConcentration = 0;
+        switch (module) {
+            case Module.A_B_AB:
+                maxConcentration = 500;
+                break;
+            case Module.A_C_AC:
+                maxConcentration = 20;
+                break;
+            case Module.A_B_C_AB_AC:
+                maxConcentration = 20;
+                break;
+        }
+        return maxConcentration;
+    };
+
+    getAgentFunction = (name: AgentName | ProductName): AgentFunction => {
+        return (
+            PreComputedSimulationData.NAME_TO_FUNCTION_MAP as Record<
+                AgentName | ProductName,
+                AgentFunction
+            >
+        )[name];
+    };
+
+    getAgentColor = (name: AgentName | ProductName): string => {
+        const agentFunction = this.getAgentFunction(name);
+        return AGENT_AND_PRODUCT_COLORS[agentFunction];
+    };
+
+    getActiveAgents = () => {
+        return [];
+    };
+
+    getInitialConcentrations = (): CurrentConcentration => {
+        return {};
+    };
+
+    createAgentsFromConcentrations = (): InputAgent[] | null => {
+        return null;
+    };
+}
