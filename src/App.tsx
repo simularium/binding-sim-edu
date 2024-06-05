723ad2d39f5a96dea38b668a2b1bb9cae1bad376
meganrm <meganrm@gmail.com>
2024-06-05 09:24:22 -0700

    make check consistent

--- src/App.tsx
+++ src/App.tsx
@@ -399,7 +399,7 @@ function App() {
                             <CenterPanel
                                 reactionType={currentModule}
                                 hasProgressed={
-                                    currentProductConcentrationArray.length > 0
+                                    currentProductConcentrationArray.length > 1
                                 }
                             />
                         }
