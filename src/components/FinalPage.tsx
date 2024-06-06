f5c489cd32c539919f8fa4dd07ee352cece94b5e
meganrm <meganrm@gmail.com>
2024-06-06 16:18:58 -0700

    style final page

--- src/components/FinalPage.tsx
+++ src/components/FinalPage.tsx
@@ -1,50 +1,22 @@
-import { useEffect, useRef, useState } from "react";
-import useWindowResize from "../hooks/useWindowResize";
 import styles from "./finalpage.module.css";
+import { Flex } from "antd";
 
 const FinalPage: React.FC = () => {
-    const shorterImage = useRef<HTMLImageElement>(null);
-    const [height, setHeight] = useState<number>(0);
-    useEffect(() => {
-        setHeight(shorterImage.current?.offsetHeight || 0);
-    }, []);
-
-    useWindowResize(() => setHeight(shorterImage.current?.offsetHeight || 0));
     return (
-        <div className={styles.container}>
+        <Flex className={styles.container}>
             <div className={styles.imageContainer}>
-                <div>
-                    <img
-                        ref={shorterImage}
-                        style={{ maxWidth: "100%" }}
-                        src="src/assets/AgAbInterface_WithInset.png"
-                        alt="hero"
-                    />
-                </div>
-                <div style={{ height: height }}>
-                    <img
-                        src="src/assets/Schematic_Hbonds_v2-strong.png"
-                        alt="hero"
-                    />
-                </div>
+                <img
+                    src="src/assets/antibody-diagram-01.png"
+                    alt="antibody bound to antigen with higher affinity"
+                />
             </div>
             <div className={styles.imageContainer}>
-                <div>
-                    <img
-                        ref={shorterImage}
-                        style={{ maxWidth: "100%" }}
-                        src="src/assets/AgAbInterface_mutated_WithInset.png"
-                        alt="hero"
-                    />
-                </div>
-                <div style={{ height: height }}>
-                    <img
-                        src="src/assets/Schematic_Hbonds_v2-weak.png"
-                        alt="hero"
-                    />
-                </div>
+                <img
+                    src="src/assets/antibody-diagram-02.png"
+                    alt="antibody bound to antigen with lower affinity"
+                />
             </div>
-        </div>
+        </Flex>
     );
 };
 
