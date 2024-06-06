bd525f2f4f60dfbff7744c5f449a1ff15c6e4d71
meganrm <meganrm@gmail.com>
2024-06-06 16:31:25 -0700

    fix alt text

--- src/components/FinalPage.tsx
+++ src/components/FinalPage.tsx
@@ -7,13 +7,13 @@ const FinalPage: React.FC = () => {
             <div className={styles.imageContainer}>
                 <img
                     src="src/assets/antibody-diagram-01.png"
-                    alt="antibody bound to antigen with higher affinity"
+                    alt="antibody bound to antigen with lower affinity"
                 />
             </div>
             <div className={styles.imageContainer}>
                 <img
                     src="src/assets/antibody-diagram-02.png"
-                    alt="antibody bound to antigen with lower affinity"
+                    alt="antibody bound to antigen with higher affinity"
                 />
             </div>
         </Flex>
