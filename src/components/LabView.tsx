c53e3ad5a8993c57eb74153e87052c22c66043c4
Megan Riel-Mehan <meganrm@gmail.com>
2024-06-28 10:56:55 -0700

    Feature/init image (#91)

--- src/components/LabView.tsx
+++ src/components/LabView.tsx
@@ -4,18 +4,24 @@ import { AGENT_AB_COLOR } from "../constants/colors";
 import { SimulariumContext } from "../simulation/context";
 import Cuvette from "./icons/Cuvette";
 import styles from "./labview.module.css";
+import classNames from "classnames";
 
 const LabView: React.FC = () => {
-    const { currentProductionConcentration, maxConcentration } =
+    const { currentProductionConcentration, maxConcentration, page } =
         useContext(SimulariumContext);
     const colorGradient = useMemo(() => {
         const rainbow = new Rainbow();
         rainbow.setSpectrum("#FFFFFF", AGENT_AB_COLOR);
         return rainbow;
-    }, [])
+    }, []);
     const position = (currentProductionConcentration / maxConcentration) * 100;
     return (
-        <div className={styles.container}>
+        <div
+            className={classNames([
+                styles.container,
+                { [styles.top]: page === 1 },
+            ])}
+        >
             <div className={styles.cuvette}>
                 <Cuvette color={colorGradient.colorAt(position)} />
             </div>
