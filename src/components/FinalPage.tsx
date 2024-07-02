bfa25b13975a12c94cba0316387aa7884e880dfb
meganrm <meganrm@gmail.com>
2024-07-02 11:12:11 -0700

    fix png urls

--- src/components/FinalPage.tsx
+++ src/components/FinalPage.tsx
@@ -1,18 +1,20 @@
-import styles from "./finalpage.module.css";
 import { Flex } from "antd";
+import diagram1Url from "../assets/antibody-diagram-01.png";
+import diagram2Url from "../assets/antibody-diagram-02.png";
+import styles from "./finalpage.module.css";
 
 const FinalPage: React.FC = () => {
     return (
         <Flex className={styles.container}>
             <div className={styles.imageContainer}>
                 <img
-                    src="src/assets/antibody-diagram-01.png"
+                    src={diagram1Url}
                     alt="antibody bound to antigen with lower affinity"
                 />
             </div>
             <div className={styles.imageContainer}>
                 <img
-                    src="src/assets/antibody-diagram-02.png"
+                    src={diagram2Url}
                     alt="antibody bound to antigen with higher affinity"
                 />
             </div>
