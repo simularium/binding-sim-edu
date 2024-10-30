bbe88804fe23f91d654c71cd29ce550711653ac9
meganrm <meganrm@gmail.com>
2024-10-30 16:54:48 -0700

    refactor

--- src/hooks/useModule.ts
+++ src/hooks/useModule.ts
@@ -1,39 +1,27 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useMemo } from "react";
 import content from "../content";
 import { Module, Section } from "../types";
 
 const useModule = (currentModule: Module) => {
-    const moduleInfo = useRef({
-        totalPages: 0,
-        totalBonusPages: 0,
-        totalMainContentPages: 0,
-        exampleTrajectoryPageNumber: -1,
-    });
-
-    const updateModuleInfo = useCallback(() => {
+    const moduleInfo = useMemo(() => {
         const contentData = content[currentModule];
-        moduleInfo.current.totalPages = contentData.length - 1;
-        moduleInfo.current.totalBonusPages = contentData.filter(
+        const totalPages = contentData.length - 1;
+        const totalBonusPages = contentData.filter(
             (page) => page.section === Section.BonusContent
         ).length;
-        moduleInfo.current.totalMainContentPages =
-            moduleInfo.current.totalPages - moduleInfo.current.totalBonusPages;
-        moduleInfo.current.exampleTrajectoryPageNumber = contentData.findIndex(
+        const totalMainContentPages = totalPages - totalBonusPages;
+        const exampleTrajectoryPageNumber = contentData.findIndex(
             (page) => page.trajectoryUrl !== undefined
         );
+
+        return {
+            totalPages,
+            totalBonusPages,
+            totalMainContentPages,
+            exampleTrajectoryPageNumber,
+        };
     }, [currentModule]);
-
-    useEffect(() => {
-        updateModuleInfo();
-    }, [updateModuleInfo]);
-
-    return {
-        totalPages: moduleInfo.current.totalPages,
-        totalBonusPages: moduleInfo.current.totalBonusPages,
-        totalMainContentPages: moduleInfo.current.totalMainContentPages,
-        exampleTrajectoryPageNumber:
-            moduleInfo.current.exampleTrajectoryPageNumber,
-    };
+    return moduleInfo;
 };
 
 export default useModule;
