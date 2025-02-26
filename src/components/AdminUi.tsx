dda7485c5bb84e7d25d506cf088f9c53cf602dd6
Megan Riel-Mehan <meganrm@gmail.com>
2025-02-26 11:20:52 -0800

    Update src/components/AdminUi.tsx
    Co-authored-by: Peyton Lee <peytonalee@gmail.com>

--- src/components/AdminUi.tsx
+++ src/components/AdminUi.tsx
@@ -57,7 +57,7 @@ const AdminUI: React.FC<AdminUIProps> = ({
                         min={0}
                         max={totalPages}
                         step={1}
-                        overRideValue={page}
+                        overrideValue={page}
                         initialValue={page}
                         onChange={(_, value): void => {
                             setPage(value);
