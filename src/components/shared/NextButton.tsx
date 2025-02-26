775276fab49c601e6d1daa4a143a636d9e4b8314
meganrm <meganrm@gmail.com>
2025-02-25 21:07:22 -0800

    go to next module

--- src/components/shared/NextButton.tsx
+++ src/components/shared/NextButton.tsx
@@ -1,14 +1,33 @@
 import { useContext } from "react";
 import { SimulariumContext } from "../../simulation/context";
 import { PrimaryButton } from "./ButtonLibrary";
+import { FIRST_PAGE } from "../../content";
 
 interface NextButtonProps {
     text?: string;
 }
 
 const NextButton = ({ text }: NextButtonProps) => {
-    const { page, setPage } = useContext(SimulariumContext);
-
+    const {
+        page,
+        setPage,
+        moduleLength,
+        module,
+        setCurrentModule,
+        setIsPlaying,
+    } = useContext(SimulariumContext);
+    if (page + 1 >= moduleLength) {
+        const goToNextModule = () => {
+            setPage(FIRST_PAGE);
+            setCurrentModule(module + 1);
+            setIsPlaying(false);
+        };
+        return (
+            <PrimaryButton onClick={goToNextModule}>
+                {text || "Finish"}
+            </PrimaryButton>
+        );
+    }
     return (
         <PrimaryButton onClick={() => setPage(page + 1)}>
             {text || "Next"}
