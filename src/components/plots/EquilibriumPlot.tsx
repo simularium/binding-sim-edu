9c5e3aa7b565c01664706403c7a62a672157f9f5
meganrm <meganrm@gmail.com>
2025-04-03 12:45:12 -0700

    revert change

--- src/components/plots/EquilibriumPlot.tsx
+++ src/components/plots/EquilibriumPlot.tsx
@@ -109,7 +109,7 @@ const EquilibriumPlot: React.FC<PlotProps> = ({
         height: Math.max(130, height),
         xaxis: {
             ...AXIS_SETTINGS,
-            range: [-10, xAxisMax],
+            range: [0, xAxisMax],
             title: `[${adjustableAgentName}] ${MICRO}M`,
             titlefont: {
                 ...AXIS_SETTINGS.titlefont,
