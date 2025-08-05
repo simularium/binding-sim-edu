423718eee2810c5daae7ae934a1bc4c445b37d9c
meganrm <meganrm@gmail.com>
2025-08-05 11:13:45 -0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -474,6 +474,7 @@ function App() {
 
     const setModule = (module: Module) => {
         setPage(FIRST_PAGE[module]);
+        clearAllAnalysisState();
         setCurrentModule(module);
         setIsPlaying(false);
     };
