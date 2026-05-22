09488469a74827a548c4212965adc48621d11de5
meganrm <meganrm@gmail.com>
2026-05-21 17:54:55 -0700

    add a Ki question

--- src/components/main-layout/CenterPanel.tsx
+++ src/components/main-layout/CenterPanel.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ViewSwitch from "../ViewSwitch";
 import EquilibriumQuestion from "../quiz-questions/EquilibriumQuestion";
 import KdQuestion from "../quiz-questions/KdQuestion";
+import KiQuestion from "../quiz-questions/KiQuestion";
 import styles from "./layout.module.css";
+import { useSimulariumUi } from "../../hooks/useSimulationContext";
+import { Module } from "../../types";
 
 interface CenterPanelProps {
     kd: number;
@@ -24,13 +27,18 @@ const CenterPanel: React.FC<CenterPanelProps> = ({
     overlay,
 }) => {
     const [lastOpened, setLastOpened] = React.useState<string | null>(null);
+    const { module } = useSimulariumUi();
     return (
         <>
             <ViewSwitch />
             <CenterPanelContext.Provider value={{ lastOpened, setLastOpened }}>
                 <div className={styles.questionContainer}>
                     <EquilibriumQuestion />
-                    <KdQuestion kd={kd} canAnswer={canDetermineEquilibrium} />
+                    {module === Module.A_B_D_AB ? (
+                        <KiQuestion ki={kd} canAnswer={canDetermineEquilibrium} />
+                    ) : (
+                        <KdQuestion kd={kd} canAnswer={canDetermineEquilibrium} />
+                    )}
                 </div>
             </CenterPanelContext.Provider>
             {overlay && overlay}
